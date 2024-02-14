5d04f171e5bc79d064a48c4c97304a0c40e4c78a
Eliecer <eliecer.sanchez47@gmail.com>
2024-02-14 19:17:03 -0400

    endpoint linkedin page fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,11 +14,11 @@ const scrollTop = () => {
   
 return (
     <footer className='relative bg-blue-950 mt-6 flex items-center justify-center gap-3 py-12' id='contact'>
-        <a href="https://github.com/neoEliecer-47" target='_blank'>
+        <a href="https://github.com/neoEliecer-47" target='_blank' rel='noreferrer'>
             <img src={github} alt="github" title="Go to Eliecer's Github page" className='h-16 w-16 lg:h-20 lg:w-20'/>
         </a>
-        <a href="https://linkedin.com/in/eliecer-sanchez-mora-22688a26a" target='_blanck'>
-            <img src={linkedin} alt="linkedin" title="Go to Eliecer's Linkedin page"  className='h-16 w-16 lg:h-20 lg:w-20'/>
+        <a href="https://www.linkedin.com/in/eliecer-smora-22688a26a/" target='_blanck' rel='noreferrer'>
+            <img src={linkedin} alt="linkedin" title="Go to Eliecer's LinkedIn page"  className='h-16 w-16 lg:h-20 lg:w-20'/>
         </a>
         <aside className=''>
         <button title='Go up' onClick={scrollTop} className='absolute p-1 hover:bg-gray-400 duration-500 bg-gray-200 top-2 rounded-lg right-2'>
